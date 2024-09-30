59cf73cb7c949c2aacc6b1e174594c621d7e2f16
Pedro Sousa Lacerda <pslacerda@gmail.com>
2024-09-30 11:40:59 -0300

    Rede de colaborações e remoção de termos

--- extrair_palavras.py
+++ extrair_palavras.py
@@ -1,8 +1,6 @@
 import textract
 import pandas as pd
-from collections import Counter
 from nltk.corpus import stopwords
-from nltk.stem.snowball import SnowballStemmer
 import nltk
 import unidecode
 
@@ -13,19 +11,28 @@ pt_stopwords = stopwords.words("portuguese") + [
     'v',
     'a',
     'e',
+    "01",
     "1",
     '0',
+    "6",
     '8',
+    "13",
     'c',
     'p.',
     'm.',
     'f.',
+    "r",
     "''",
     '``',
     "...",
-] + [c for c in '.,-–()":*a“”;[]%#&;;;\'!<>']
-
-stemmer = SnowballStemmer("portuguese")
+    "'s",
+    "k.",
+    "l.",
+    "nao",
+    "2022.",
+    "2023",
+    "cm",
+] + [c for c in '.,-––()":*a“”;[]%#&;;;\'!@<>=?1234567890$']
 
 
 T = pd.read_csv("trabalhos.csv")
@@ -37,27 +44,47 @@ def process_words_1gram(words):
         w for w in words
         if w not in pt_stopwords
     )
-    # words = [stemmer.stem(w) for w in words]
     return words
 
 
 def process_words_2gram(words):
-    bigrams = nltk.bigrams(words)
-    bigrams = (
-        '%s %s' % (w1, w2) for w1, w2 in bigrams
-        if w1 not in pt_stopwords and w2 not in pt_stopwords
-    )
-    return [b for b in bigrams if b not in [
-            "de uma",
-            "0 0",
-            "= >",
-            ") –"
-        ]]
-
+    return [
+        '%s %s' % (w1, w2) for w1, w2 in nltk.bigrams(words)
+        if w1 not in pt_stopwords and w2 not in pt_stopwords and 
+            '%s %s' % (w1, w2) not in [
+                "alem disso",
+                "dessa forma",
+                "desta forma",
+                "desse modo",
+                "nesse sentido",
+                "muitas vezes",
+                "nesse contexto",
+                "et al",
+                "0 0",
+                "campus sao",
+                "etapas construtivas",
+                "fazem parte",
+                "culturalmente relevante",
+                "arquivo pessoal",
+                "knijnik et",
+                "primeira autora",
+                "acervo proprio"
+            ]
+    ]
 
 for i, row in T.iterrows():
     pdf = row['Pdf']
     words = textract.process(pdf).decode()
+    words = words[words.index('\n\n\x0c'):]
+    for chave in ['REFERÊNCIAS', 'REFERENCIAS', 'REFERENCIAL BIBLIOGRÁFICO', 'COM QUEM VOAMOS', 'REFERÊNCIA', 'Referências', 'Referência']:
+        try:
+            words = words[:words.index(chave)]
+            break
+        except ValueError:
+            continue
+    else:
+        raise
+
     words = unidecode.unidecode(words).lower()
     tokens = nltk.tokenize.word_tokenize(words, language='portuguese')    
 
@@ -75,7 +102,6 @@ for i, row in T.iterrows():
             **base_row,
             'Tipo': 'Unigram',
             'Termo': termo,
-            # 'Freq': len([t == termo for t in unigrams]) / len(unigrams)
         })
 
     for termo in bigrams:
@@ -83,7 +109,6 @@ for i, row in T.iterrows():
             **base_row,
             'Tipo': 'Bigram',
             'Termo': termo,
-            'Freq': len([t == termo for t in bigrams]) / len(bigrams)
         })
 (
     pd.DataFrame
